4d0a606781cce93bb21339b082625ce933dcfd1e
Christopher Peplin <peplin@bueda.com>
2011-03-07 18:21:00 -0500

    Add JW Player for embedded Flash/HTML5 playback.

--- public/js/astral.js
+++ public/js/astral.js
@@ -4,3 +4,13 @@ WebFont.load({
         urls: ['/css/font.css']
     }
 });
+
+jwplayer("player").setup({
+    players: [
+        {type: "flash", src: "/swf/vendor/player.swf"},
+        {type: "html5"}
+    ],
+    file: "/video.mp4",
+    height: 270,
+    width: 480
+});
