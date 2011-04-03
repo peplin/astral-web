50d0f324d77e9e9df825691fdc6cadaf52efc173
Christopher Peplin <peplin@bueda.com>
2011-04-03 19:14:48 -0400

    Use a string UUID instead of int id for nodes.

--- models/node.rb
+++ models/node.rb
@@ -2,10 +2,9 @@ require 'dm-core'
 
 class Node
   include DataMapper::Resource
-  property :id, Serial, :required => true
+  property :uuid, String, :required => true, :unique => true, :key => true
   property :ip_address, String, :required => true
   property :port, Integer, :required => true
-  property :uuid, Integer, :required => true, :unique => true
   property :supernode, Boolean, :default => false
   property :created_at, DateTime
   property :updated_at, DateTime
